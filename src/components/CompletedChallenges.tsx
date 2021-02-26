6c9960a8b1cd7fd71e63780414b71a1dfe9cdb38
cledson <cledman@gmail.com>
2021-02-25 23:59:15 -0300

    Challenges part created

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,13 +1,16 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges() {
+    const {challengesCompleted} = useContext(ChallengeContext);
     return (
         <div className={styles.completedChallengesContainer}>
             <span>
                 Desafios completos
             </span>
             <span>
-                5
+                {challengesCompleted}
             </span>            
         </div>
     );
